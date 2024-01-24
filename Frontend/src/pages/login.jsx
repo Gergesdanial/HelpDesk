8a9bf402af9406fc5544ac29f13a620665b98adc
Gerges Makarious Danial <gergesdanial11@outlook.com>
2024-01-24 18:52:19 +0200

    n

---
+++ Frontend/src/pages/login.jsx
@@ -0,0 +1,105 @@
+import "../stylesheets/Login.css";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const backend_url = "http://localhost:3000/api/v1";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { email, password } = inputValue;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${backend_url}/login`,
+        { ...inputValue },
+        {  withCredentials: true, credentials: 'include'  }
+      );
+  
+      const { status, data } = response;
+      if (status === 200) {
+        const { token, user } = data;
+  
+        // Save the token in the headers for future API requests
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  
+        // Save the token in a cookie
+        document.cookie = `token=${token}; path=/; samesite=None;`;
+       
+        // Save other user information if needed
+        localStorage.setItem("userId", user._id);
+        localStorage.setItem("role", user.role);
+        await axios.post(`${backend_url}/otp/send-otp/${email}`, {},{withCredentials: true});
+
+        navigate("/otp", {state:{email: inputValue.email}}); // Redirect to Admin.jsx
+
+      } else {
+        setErrorMessage("Invalid email or password");
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Server error");
+    }
+    setInputValue({
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <div className="login">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="h1">Login</div>
+        <input
+          pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
+          placeholder="Email"
+          id="email"
+          name="email"
+          type="text"
+          className="input form-content"
+          value={email}
+          onChange={handleOnChange}
+        />
+        <input
+          placeholder="Password"
+          id="password"
+          name="password"
+          type="password"
+          className="input form-content"
+          value={password}
+          onChange={handleOnChange}
+        />
+        <input value="Login" className="btn" type="submit" />
+        <span className="sign-up-label">{errorMessage}</span>
+      </form>
+
+      <div className="rays">{/* SVG code for rays */}</div>
+
+      <div className="emitter">{/* SVG code for emitter */}</div>
+
+      <div className="buttons-container">
+        <span className="sign-up-label">Don't have an account?</span>
+        <Link className="sign-up-link" to={"/signup"}>
+          Signup
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ README.md
@@ -0,0 +1,10 @@
+# SwPoject
+ 
+To run backend type : cd backend
+npm install
+mpm start
+to run frontend : cd frontend
+npm install vite --save-dev
+npm run dev 
+
+Don't forget to copy the path of questionState.js and paste it in showchat.jsx and livechat.jsx
